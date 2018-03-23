b8193bfd81573c709cfed3eaa43f33bad9d54f43
nmandrelas <nmandrelas@gmail.com>
2018-03-23 21:51:57 +0200

    -- Trying to create tests

---
+++ src/test/java/com/compilers/proj/CustomTests.java
@@ -0,0 +1,12 @@
+package com.compilers.proj;
+
+import junit.framework.TestCase;
+
+public class CustomTests extends TestCase{
+
+    public void testStrings() {
+        Lexer scanner = null;
+        assertEquals(2, true);
+    }
+
+}
